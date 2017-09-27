e25b1d02e130f7b7736070faa88a032a2cc3f5ca
lukas <lukas.diem@gmail.com>
2017-09-27 23:23:22 +0200

    added json generation

--- create_svg.py
+++ create_svg.py
@@ -6,11 +6,11 @@ from matplotlib.collections import PolyCollection
 from path import Path
 
 from wl2generator.voronoi_graph import VoronoiGraph
-from wl2generator.voronoi_map import create_map, polygons_from_map
+from wl2generator.voronoi_map import create_map, polygons_from_map, map_to_json
 
 
 def create_plot(map_dict, vor, graph_size):
-    poly_continents, poly_countries = polygons_from_map(vor, map_dict)
+    poly_continents, poly_countries, _ = polygons_from_map(vor, map_dict)
 
     plt.figure()
     off = 100
@@ -41,10 +41,15 @@ if __name__ == '__main__':
         vg_pts = vg.relax_points(times=2)
         vor = vg.vor
 
-        map_dict = create_map(vg, (2, 8), (10, 15), (20, 70))
+        #map_dict = create_map(vg, (2, 8), (10, 15), (20, 70))
+        map_dict = create_map(vg, (2, 4), (3, 8), (20, 70))
         print('Map creation took: {:.4f}s'.format(time.time() - ts))
 
+        map_to_json(vor, map_dict)
         create_plot(map_dict, vor, graph_size)
+        plt.show()
+
+
         plt.savefig(out_path / 'map_{:03d}.png'.format(idx), dpi=300, transparent=True,
                     bbox_inches='tight', pad_inches=0, facecolor=(0, 0.4, 0.55, 0.8))
         plt.savefig(out_path / 'map_{:03d}.svg'.format(idx), dpi=300, transparent=True,
